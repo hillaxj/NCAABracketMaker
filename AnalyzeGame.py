6984eaf99c0f5e8f41fd1bd4a6f8730d9c444540
hillaxj <hillaxj@gmail.com>
2021-03-08 18:03:52 -0500

    Finalized win algo based only on wins vs losses

--- AnalyzeGame.py
+++ AnalyzeGame.py
@@ -1,4 +1,22 @@
 import logging as log
-import re
 from utilities import datapath
+import pandas as pd
 
+
+def whoWins(team1, team2):
+    # Selects winner based on team records
+
+    # Pulls each team's wins and losses from TeamData.csv
+    df = pd.read_csv(datapath + 'TeamData.csv', index_col='Team Name')
+    team1wins = df.loc[team1, 'Team Win Record']
+    team1losses = df.loc[team1, 'Team Loss Record']
+    team2wins = df.loc[team2, 'Team Win Record']
+    team2losses = df.loc[team2, 'Team Loss Record']
+
+    # Compares wins and losses, tie goes to team2
+    if team1wins > team2wins and team1losses < team2losses:
+        winner = team1
+    else:
+        winner = team2
+
+    return winner
