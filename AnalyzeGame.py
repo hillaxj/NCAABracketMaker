0c591a95f232a8e7799bd7d0cb0fc133eec23c12
hillaxj <hillaxj@gmail.com>
2021-03-10 12:56:18 -0500

    Included schedule strengh into whoWins. Corrected scheduleStrength and added adjustment factors

--- AnalyzeGame.py
+++ AnalyzeGame.py
@@ -1,6 +1,7 @@
 from utilities import datapath
 import pandas as pd
 import ast
+import logging as log
 
 
 def whoWins(team1, team2, teamdatafile):
@@ -10,9 +11,11 @@ def whoWins(team1, team2, teamdatafile):
     df = pd.read_csv(datapath + teamdatafile, index_col='Team Name')
     team1Ratio = df.at[team1, 'Team Win Ratio']
     team2Ratio = df.at[team2, 'Team Win Ratio']
+    team1Schedule = df.at[team1, 'Schedule Strength']
+    team2Schedule = df.at[team2, 'Schedule Strength']
 
     # Compares wins and losses, tie goes to team2
-    if team1Ratio >= team2Ratio:
+    if team1Ratio * team1Schedule >= team2Ratio * team2Schedule:
         winner = team1
     else:
         winner = team2
@@ -20,26 +23,36 @@ def whoWins(team1, team2, teamdatafile):
     return winner
 
 
-def scheduleStrength(teamdatafile):
-    # Calculates value for schedule difficulty, need to tweak a lot
+def scheduleStrength(teamdatafile, winfactor, rankfactor, pointsfactor, schedulefactor):
+    # Calculates value for schedule difficulty, need to tweak a lot. Add factor for team rank and points spread
     # Reads csv file
     df = pd.read_csv(datapath + teamdatafile, index_col='Team Name')
     teams = df.index.tolist()
-    strength = 1
-    opponentStrength = []
     strlist = []
 
     for team in teams:
         # Creates dict of games played
         schedule = ast.literal_eval(df.at[team, 'Team Schedule Results'])
-        # Iterates through games played and calculates strength based on opponent record
+        opponentStrength = []
+        # Iterates through games played and calculates strength
         for i in range(1, len(schedule) + 1):
             try:
-                opponentStrength.append(df.at[schedule.get(i)[1], 'Team Win Ratio'])
-                strength = sum(opponentStrength) / len(opponentStrength)
+                # Opponent win ratio from csv
+                opponentWinRatio = df.at[schedule.get(i)[1], 'Team Win Ratio']
+                # Opponent rank factor
+                if schedule.get(i)[2] != 'N/A':
+                    opponentRank = (26-int(schedule.get(i)[2])) / 25
+                else:
+                    opponentRank = 0
+                # Point differential factor
+                gamePoints = (100 - abs(int(schedule.get(i)[4]) - int(schedule.get(i)[5])))/100
+                opponentStrength.append((winfactor * opponentWinRatio + rankfactor*opponentRank + \
+                                         pointsfactor * gamePoints)/schedulefactor)
+
             except:
                 continue
-        strlist.append(strength)
+
+        strlist.append(sum(opponentStrength) / len(opponentStrength))
     df['Schedule Strength'] = strlist
     df.to_csv(datapath + teamdatafile)
 
