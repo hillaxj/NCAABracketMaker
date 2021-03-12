fb8ce3cf4b49124b05224401b35d00915f25e70e
hillaxj <hillaxj@gmail.com>
2021-03-11 23:26:12 -0500

    Changed output seed values to match past yaml formats

--- BracketBuilder.py
+++ BracketBuilder.py
@@ -2,7 +2,7 @@ from utilities import bracketpath
 import yaml
 from AnalyzeGame import whoWins
 from math import pow
-from fpdf import FPDF
+import PyPDF2
 
 
 def roundResults(teams, round, teamdatafile):
@@ -10,10 +10,12 @@ def roundResults(teams, round, teamdatafile):
     winners = {}
 
     # Loops through each game by region
+
     for x in range(1, 5):
         for y in range(1, int(pow(2, 4-round) + 1)):
-            winners['r' + str(x) + 'seed' + str(y)] = whoWins(teams.get('r' + str(x) + 'seed' + str(y)), \
-                                                              teams.get('r' + str(x) + 'seed' + str(int(pow(2, 5-round) + 1-y))), teamdatafile)
+            winners['d' + str(round+1) + 'r' + str(x) + 'seed' + str(y)] = whoWins(teams.get('d' + str(round) + 'r' + str(x) + 'seed' + str(y)), \
+                                                              teams.get('d' + str(round) + 'r' + str(x) + 'seed' + str(int(pow(2, 5-round) \
+                                                                                                + 1-y))), teamdatafile)
 
     return winners
 
@@ -26,11 +28,11 @@ def bracketSim(bracketfile, teamdatafile):
 
     # First 4 games
     try:
-        data['r1seed16'] = whoWins(data.get('r1seed16a'), data.get('r1seed16b'), teamdatafile)
-        data['r1seed12'] = whoWins(data.get('r1seed12a'), data.get('r1seed12b'), teamdatafile)
-        data['r4seed16'] = whoWins(data.get('r4seed16a'), data.get('r4seed16b'), teamdatafile)
-        data['r4seed12'] = whoWins(data.get('r4seed12a'), data.get('r4seed12b'), teamdatafile)
-        print(data.get('r1seed16'), data.get('r1seed12'), data.get('r4seed16'), data.get('r4seed12'))
+        data['d1r1seed16'] = whoWins(data.get('d1r1seed16a'), data.get('d1r1seed16b'), teamdatafile)
+        data['d1r1seed12'] = whoWins(data.get('d1r1seed12a'), data.get('d1r1seed12b'), teamdatafile)
+        data['d1r4seed16'] = whoWins(data.get('d1r4seed16a'), data.get('d1r4seed16b'), teamdatafile)
+        data['d1r4seed12'] = whoWins(data.get('d1r4seed12a'), data.get('d1r4seed12b'), teamdatafile)
+        print(data.get('d1r1seed16'), data.get('d1r1seed12'), data.get('d1r4seed16'), data.get('d1r4seed12'))
     except:
         pass
 
@@ -39,8 +41,8 @@ def bracketSim(bracketfile, teamdatafile):
     round2winners = roundResults(round1winners, 2, teamdatafile)
     round3winners = roundResults(round2winners, 3, teamdatafile)
     round4winners = roundResults(round3winners, 4, teamdatafile)
-    round5winners = [whoWins(round4winners.get('r1seed1'), round4winners.get('r4seed1'), teamdatafile), \
-                     whoWins(round4winners.get('r2seed1'), round4winners.get('r3seed1'), teamdatafile)]
+    round5winners = [whoWins(round4winners.get('d5r1seed1'), round4winners.get('d5r4seed1'), teamdatafile), \
+                     whoWins(round4winners.get('d5r2seed1'), round4winners.get('d5r3seed1'), teamdatafile)]
     champion = whoWins(round5winners[0], round5winners[1], teamdatafile)
 
     print(round1winners)
@@ -53,22 +55,13 @@ def bracketSim(bracketfile, teamdatafile):
     return None
 
 
+
+
 def popBracket():
-    pdf_h = 216
-    pdf_w = 279
-    lineheight = pdf_h / 34
-    linelength = pdf_w / 12
-    pdf = FPDF(orientation='L', unit='mm', format='Letter')
-    pdf.add_page()
 
-    for i in range(1, 17):
-        pdf.line(2, lineheight * i, linelength + 2, lineheight * i)
-    pdf.set_font("Arial", size=15)
-    # pdf.cell(200, 10, txt="this is the programming of creating pdf file", ln=1, align="L")
+    pdf = open(bracketpath + 'NCAA Bracket clean.pdf')
+    readpdf = PyPDF2.PdfFileReader(pdf)
 
 
-
-    #
-    pdf.output('test.pdf')
 
     return None
