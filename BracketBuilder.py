35efbced0cec2e2e51f146c8c27daafc8c4fc648
hillaxj <hillaxj@gmail.com>
2021-03-14 18:51:43 -0400

    Moved where dataframe is created to speed up funciton, changed starting seed games

--- BracketBuilder.py
+++ BracketBuilder.py
@@ -1,11 +1,12 @@
-from utilities import bracketpath, simbracketpath
+from utilities import bracketpath, simbracketpath, datapath
 import yaml
 from AnalyzeGame import whoWins
 from math import pow
 import PyPDF2
+import pandas as pd
 
 
-def roundResults(teams, round, teamdatafile):
+def roundResults(teams, round, teamdatadf):
     # Tests each game for defined round
     winners = {}
 
@@ -13,9 +14,9 @@ def roundResults(teams, round, teamdatafile):
 
     for x in range(1, 5):
         for y in range(1, int(pow(2, 4-round) + 1)):
-            winners['d' + str(round+1) + 'r' + str(x) + 'seed' + str(y)] = \
-                whoWins(teams.get('d' + str(round) + 'r' + str(x) + 'seed' + str(y)), \
-                teams.get('d' + str(round) + 'r' + str(x) + 'seed' + str(int(pow(2, 5-round) + 1-y))), teamdatafile)
+            seedid = f'd{round}r{x}seed'
+            winners[f'd{round+1}r{x}seed{y}'] = whoWins(teams.get(''.join([seedid, str(y)])), \
+                                            teams.get(''.join([seedid, str(int(pow(2, 5-round) + 1-y))])), teamdatadf)
 
     return winners
 
@@ -27,6 +28,7 @@ def bracketSim(bracketfile, teamdatafile):
         print('Coronavirus')
         return None
 
+    teamdatadf = pd.read_csv(datapath + teamdatafile, index_col='Team Name')
     # Simulates all games in the supplied brackets based on teamdatafile info
     with open(bracketpath + bracketfile) as f:
         dataYaml = yaml.load(f, Loader=yaml.FullLoader)
@@ -35,23 +37,23 @@ def bracketSim(bracketfile, teamdatafile):
 
     # First 4 games
     try:
-        data['d1r1seed16'] = whoWins(data.get('d1r1seed16a'), data.get('d1r1seed16b'), teamdatafile)
-        data['d1r1seed12'] = whoWins(data.get('d1r1seed12a'), data.get('d1r1seed12b'), teamdatafile)
-        data['d1r4seed16'] = whoWins(data.get('d1r4seed16a'), data.get('d1r4seed16b'), teamdatafile)
-        data['d1r4seed12'] = whoWins(data.get('d1r4seed12a'), data.get('d1r4seed12b'), teamdatafile)
+        data['d1r1seed16'] = whoWins(data.get('d1r1seed16a'), data.get('d1r1seed16b'), teamdatadf)
+        data['d1r1seed11'] = whoWins(data.get('d1r1seed11a'), data.get('d1r1seed11b'), teamdatadf)
+        data['d1r4seed16'] = whoWins(data.get('d1r4seed16a'), data.get('d1r4seed16b'), teamdatadf)
+        data['d1r4seed11'] = whoWins(data.get('d1r4seed11a'), data.get('d1r4seed11b'), teamdatadf)
 
     except:
         pass
 
     # Simulates and stores each round until winner is determined
     champion = {}
-    round1winners = roundResults(data, 1, teamdatafile)
-    round2winners = roundResults(round1winners, 2, teamdatafile)
-    round3winners = roundResults(round2winners, 3, teamdatafile)
-    round4winners = roundResults(round3winners, 4, teamdatafile)
-    round5winners = {'d6r1seed1': whoWins(round4winners.get('d5r1seed1'), round4winners.get('d5r4seed1'), teamdatafile), \
-                     'd6r1seed2': whoWins(round4winners.get('d5r2seed1'), round4winners.get('d5r3seed1'), teamdatafile)}
-    champion['d7r1seed1'] = whoWins(round5winners.get('d6r1seed1'), round5winners.get('d6r1seed2'), teamdatafile)
+    round1winners = roundResults(data, 1, teamdatadf)
+    round2winners = roundResults(round1winners, 2, teamdatadf)
+    round3winners = roundResults(round2winners, 3, teamdatadf)
+    round4winners = roundResults(round3winners, 4, teamdatadf)
+    round5winners = {'d6r1seed1': whoWins(round4winners.get('d5r1seed1'), round4winners.get('d5r4seed1'), teamdatadf), \
+                     'd6r1seed2': whoWins(round4winners.get('d5r2seed1'), round4winners.get('d5r3seed1'), teamdatadf)}
+    champion['d7r1seed1'] = whoWins(round5winners.get('d6r1seed1'), round5winners.get('d6r1seed2'), teamdatadf)
 
     try:
         data.pop('d1r1seed16a')
