a25927424359ceb32b2370f9841efd4e57aea894
Alex Hill <hillaxj@gmail.com>
2022-03-15 13:54:26 -0400

    Added try statement for first 4 games

--- NCAABracketMaker/BracketBuilder.py
+++ NCAABracketMaker/BracketBuilder.py
@@ -2,11 +2,10 @@ from NCAABracketMaker.utilities import bracketpath, simbracketpath, teampath, mo
 import yaml
 from NCAABracketMaker.AnalyzeGame import whoWins
 from math import pow
-import pdfrw
 import pandas as pd
 import os
 from openpyxl import load_workbook
-
+# TODO : Add function to import current year bracket into yaml ie: NCAAMBracket2021.yaml
 
 def roundResults(teams, round, teamdatadf, pointcof, wincof, rankcof, ratiocof):
     # Tests each game for defined round
@@ -17,7 +16,7 @@ def roundResults(teams, round, teamdatadf, pointcof, wincof, rankcof, ratiocof):
     for x in range(1, 5):
         for y in range(1, int(pow(2, 4-round) + 1)):
             seedid = f'd{round}r{x}seed'
-            winners[f'd{round+1}r{x}seed{y}'] = whoWins(teams.get(''.join([seedid, str(y)])), \
+            winners[f'd{round+1}r{x}seed{y}'] = whoWins(teams.get(''.join([seedid, str(y)])),
                                             teams.get(''.join([seedid, str(int(pow(2, 5-round) + 1-y))])), teamdatadf, pointcof, wincof, rankcof, ratiocof)
 
     return winners
@@ -39,15 +38,43 @@ def bracketSim(bracketfile, teamdatafile, pointcof, wincof, rankcof, ratiocof):
     # bracketData = pd.read_csv(bracketpath + bracketfile)
 
     data = dict((k, v) for k, v in bracketData.items() if k[:2] == 'd1')
-    print(data.get('d1r1seed16a'))
+    print(data.get('d1r2seed16a'))
 
-    # First 4 games
+    # First 4 games, different years have different first 4 games
     try:
         data['d1r1seed16'] = whoWins(data.get('d1r1seed16a'), data.get('d1r1seed16b'), teamdatadf, pointcof, wincof, rankcof, ratiocof)
+    except:
+        pass
+    try:
         data['d1r1seed11'] = whoWins(data.get('d1r1seed11a'), data.get('d1r1seed11b'), teamdatadf, pointcof, wincof, rankcof, ratiocof)
+    except:
+        pass
+    try:
+        data['d1r2seed16'] = whoWins(data.get('d1r2seed16a'), data.get('d1r2seed16b'), teamdatadf, pointcof, wincof, rankcof, ratiocof)
+    except:
+        pass
+    try:
+        data['d1r2seed11'] = whoWins(data.get('d1r2seed11a'), data.get('d1r2seed11b'), teamdatadf, pointcof, wincof, rankcof, ratiocof)
+    except:
+        pass
+    try:
+        data['d1r3seed16'] = whoWins(data.get('d1r3seed16a'), data.get('d1r3seed16b'), teamdatadf, pointcof, wincof, rankcof, ratiocof)
+    except:
+        pass
+    try:
+        data['d1r3seed11'] = whoWins(data.get('d1r3seed11a'), data.get('d1r3seed11b'), teamdatadf, pointcof, wincof, rankcof, ratiocof)
+    except:
+        pass
+    try:
         data['d1r4seed16'] = whoWins(data.get('d1r4seed16a'), data.get('d1r4seed16b'), teamdatadf, pointcof, wincof, rankcof, ratiocof)
+    except:
+        pass
+    try:
         data['d1r4seed11'] = whoWins(data.get('d1r4seed11a'), data.get('d1r4seed11b'), teamdatadf, pointcof, wincof, rankcof, ratiocof)
-
+    except:
+        pass
+    try:
+        data['d1r4seed12'] = whoWins(data.get('d1r4seed12a'), data.get('d1r4seed12b'), teamdatadf, pointcof, wincof, rankcof, ratiocof)
     except:
         pass
 
@@ -57,7 +84,7 @@ def bracketSim(bracketfile, teamdatafile, pointcof, wincof, rankcof, ratiocof):
     round2winners = roundResults(round1winners, 2, teamdatadf, pointcof, wincof, rankcof, ratiocof)
     round3winners = roundResults(round2winners, 3, teamdatadf, pointcof, wincof, rankcof, ratiocof)
     round4winners = roundResults(round3winners, 4, teamdatadf, pointcof, wincof, rankcof, ratiocof)
-    round5winners = {'d6r1seed1': whoWins(round4winners.get('d5r1seed1'), round4winners.get('d5r4seed1'), teamdatadf, pointcof, wincof, rankcof, ratiocof), \
+    round5winners = {'d6r1seed1': whoWins(round4winners.get('d5r1seed1'), round4winners.get('d5r4seed1'), teamdatadf, pointcof, wincof, rankcof, ratiocof),
                      'd6r1seed2': whoWins(round4winners.get('d5r2seed1'), round4winners.get('d5r3seed1'), teamdatadf, pointcof, wincof, rankcof, ratiocof)}
     champion['d7r1seed1'] = whoWins(round5winners.get('d6r1seed1'), round5winners.get('d6r1seed2'), teamdatadf, pointcof, wincof, rankcof, ratiocof)
 
@@ -88,7 +115,7 @@ def bracketSim(bracketfile, teamdatafile, pointcof, wincof, rankcof, ratiocof):
 
 
 def populateBracket(simbracket):
-
+    # TODO: Fix function to export results into excel sheet
     book = load_workbook(f'{simbracketpath}Sim_Bracket.xlsx')
     writer = pd.ExcelWriter(f'{simbracketpath}Sim_Bracket.xlsx', engine='openpyxl')
     writer.book = book
