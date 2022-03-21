d59c41b584913484d9ff392d71aeccdec0cd223e
Alex Hill <hillaxj@gmail.com>
2022-03-21 09:20:19 -0400

    Created example.py file

---
+++ NCAABracketMaker/Example.py
@@ -0,0 +1,20 @@
+"""
+Call on functions to pull data and create brackets
+"""
+from NCAABracketMaker.BracketBuilder import bracketmaker
+from NCAABracketMaker.TeamData import getemptybracket
+
+# Example values populated below
+sex = 'mens'        # Must be 'mens' or 'womens'
+year = 2022         # Empty brackets available for 2021 and 2022 only
+
+win = 1             # Recommend: Float between 0 and 1
+rank = 1            # Recommend: Float between 0 and 1
+points = .5         # Recommend: Float between 0 and 1
+schedule = 5        # Recommend: Float between 1 and 3
+
+reset = False
+
+# Imports data from current year and simulates bracket with defined parameters. Open Sim_Bracket.xlsx to see results.
+# Can clear simulated brackets by setting reset to True
+bracketmaker(sex, year, win, rank, points, schedule, reset)
