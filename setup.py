4e4e075fbe74eb9145f2a5d8e81c377a2c254808
Alex Hill <hillaxj@gmail.com>
2022-03-23 12:18:35 -0400

    Updated version to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='NCAABracketMaker',
-    version='1.0',
+    version='1.1',
     packages=['NCAABracketMaker'],
     package_dir={'NCAABracketMaker': 'NCAABracketMaker'},
     package_data={'NCAABracketMaker': [
