fc58d55da8e2085115f67cc1dce70648b7ffe743
Alex Hill <hillaxj@gmail.com>
2022-04-12 16:49:04 -0400

    Removed old comments

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
 from setuptools import setup
 
 setup()
-#
-# if __name__ == "__main__":
-#     setuptools.setup(
-#         use_scm_version=True,
-#         setup_requires=['setuptools_scm']
-#     )
