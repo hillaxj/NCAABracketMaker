fe09daf58f6018077cbf927b7e44c1909bd55638
Alex Hill <hillaxj@gmail.com>
2022-04-12 21:16:00 -0400

    Updated code to copy files to different directory

--- NCAABracketMaker/utilities.py
+++ NCAABracketMaker/utilities.py
@@ -8,38 +8,33 @@ import shutil
 log.basicConfig(level=log.INFO, format='%(asctime)s %(levelname)-8s %(message)s',
                 datefmt='%Y-%m-%d %H:%M:%S', stream=sys.stdout)
 
-source_dir = str(os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')) + '/'
-modulepath = source_dir
-
 # define local path to repo
-def get_savepath(local_dir):
-    global modulepath
-    try:
-        if local_dir != str(os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')) + '/':
-            modulepath = local_dir + '/BracketMaker'
-        else:
-            modulepath = local_dir
-    except NameError:
-        modulepath = 'NCAABracketMaker/'
+try:
+    modulepath = str(os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')) + '/'
+    deskpath = os.environ['HOME'] + '/Desktop/BracketMaker/'
+except NameError:
+    modulepath = 'NCAABracketMaker/'
+    deskpath = 'NCAABracketMaker/'
 
 
-# define package directories
-teampath = source_dir + 'TeamData/'
-bracketpath = source_dir + 'Brackets/'
-simbracketpath = modulepath + 'SimBrackets/'
+# define package directorie paths
+teampath = modulepath + 'TeamData/'
+bracketpath = modulepath + 'Brackets/'
+simbracketpath = deskpath + 'SimBrackets/'
 
-# Creates new directories
-destination_dir = modulepath
+# Creates new directory for deskpath if needed
+destination_dir = deskpath
 os.makedirs(destination_dir, exist_ok=True)
 
+# Set up to move multiple folders to deskpath if needed, only moving SimBracket for now
 folder_list = ['SimBrackets/']
 # Fetch all files
 for folder in folder_list:
     # Creates directory if needed
     os.makedirs(destination_dir + folder, exist_ok=True)
-    for file_name in os.listdir(source_dir + folder):
+    for file_name in os.listdir(modulepath + folder):
         # Construct full file path
-        source = source_dir + folder + file_name
+        source = modulepath + folder + file_name
         destination = destination_dir + folder + file_name
         # Move only files
         if os.path.exists(source) and not os.path.exists(destination):
