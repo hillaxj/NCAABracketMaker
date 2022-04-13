a18f647d14ecd1baeafc62e373a4e7668758ad2a
Alex Hill <hillaxj@gmail.com>
2022-04-12 22:41:32 -0400

    Changed save directory

--- NCAABracketMaker/utilities.py
+++ NCAABracketMaker/utilities.py
@@ -11,7 +11,7 @@ log.basicConfig(level=log.INFO, format='%(asctime)s %(levelname)-8s %(message)s'
 # define local path to repo
 try:
     modulepath = str(os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')) + '/'
-    deskpath = os.environ['HOME'] + '/Desktop/BracketMaker/'
+    deskpath = os.environ['HOME'] + '/Desktop/'
 except NameError:
     modulepath = 'NCAABracketMaker/'
     deskpath = 'NCAABracketMaker/'
