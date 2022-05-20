629b5fe703ed196fe5a6431239174321ab89614e
alex <hillaxj@gmail.com>
2022-05-20 16:48:13 -0400

    Corrected directory access

--- tests/test_NCAABracketMaker.py
+++ tests/test_NCAABracketMaker.py
@@ -1,17 +1,26 @@
 from src.NCAABracketMaker import bracketmaker, compareResults
 from src.NCAABracketMaker.utilities import simbracketpath
+import os
+
+try:
+    testpath = (
+        str(os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")) + "/"
+    )
+
+except NameError:
+    testpath = "tests/"
 
 
 def test_brackets():
     league = 'mens'  # Must be 'mens' or 'womens'
-    year = 2022  # Empty brackets available for 2021 and 2022 only
+    year = 2021  # Empty brackets available for 2021 and 2022 only
 
-    win = 1  # Recommend: Float between 0 and 1
-    rank = 1  # Recommend: Float between 0 and 1
-    points = .5  # Recommend: Float between 0 and 1
-    schedule = 5
+    win = 1.0  # Recommend: Float between 0 and 1
+    rank = 1.0  # Recommend: Float between 0 and 1
+    points = 1.0  # Recommend: Float between 0 and 1
+    schedule = 1.0
 
     bracketmaker(league, year, win, rank, points, schedule)
-    assert compareResults("test/testsim.csv", f'{simbracketpath}{league}{year}-{win}-{rank}-{points}-{schedule}-Sim.csv') \
+    assert compareResults(f'{testpath}testsim.csv', f'{simbracketpath}{league}{year}-{win}-{rank}-{points}-{schedule}-Sim.csv') \
            == 1, "Should be 1"
 
