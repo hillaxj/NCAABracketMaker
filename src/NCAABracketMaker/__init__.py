846bc1413041a5c5710e3ad6f78c60d1100fbf54
alex <hillaxj@gmail.com>
2022-05-20 16:47:54 -0400

    Renamed main

--- src/NCAABracketMaker/__init__.py
+++ src/NCAABracketMaker/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "1.1.0"
-from src.NCAABracketMaker.__main__ import bracketmaker
+from src.NCAABracketMaker.main import bracketmaker
 from src.NCAABracketMaker.MLCoefficients import compareResults
 __all__ = [bracketmaker, compareResults]
