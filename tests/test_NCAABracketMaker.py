c16a45a945b641689d64c8c84c4b030418ad02a0
alex <hillaxj@gmail.com>
2022-05-25 23:43:02 -0400

    test file

--- tests/test_NCAABracketMaker.py
+++ tests/test_NCAABracketMaker.py
@@ -26,3 +26,19 @@ def test_brackets():
                           f'{simbracketpath}{league}{year}-{win}-{rank}-{points}-{schedule}'
                           f'-Sim.csv') == 1, "Output changed from original algorithm"
 
+
+# def test_inputs(caplog):
+#     league = 'mens'  # Must be 'mens' or 'womens'
+#     year = 2021  # Empty brackets available for 2021 and 2022 only
+#
+#     win = 1.0  # Recommend: Float between 0 and 1
+#     rank = 1.0  # Recommend: Float between 0 and 1
+#     points = 1.0  # Recommend: Float between 0 and 1
+#     schedule = 1.0
+#     # with pytest.raises(ValueError) as exc_info:
+#     bracketmaker('Fish', year, win, rank, points, schedule)
+#     out = caplog.text
+#     assert out == 'Not a valid league. Must be either "mens" or "womens"\n'
+#     # exception_raised = exc_info.value
+#     # assert exception_raised == ValueError
+#     # bracketmaker(league, year, win, rank, points, schedule)
