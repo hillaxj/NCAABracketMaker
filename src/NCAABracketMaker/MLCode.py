47820f715ce3bef72c5b4c69e4c8f8d28849316e
alex <hillaxj@gmail.com>
2022-08-25 18:10:39 -0400

    Added ml functions

--- src/NCAABracketMaker/MLCode.py
+++ src/NCAABracketMaker/MLCode.py
@@ -54,9 +54,10 @@ def compareResults(results, simbracket):
     return percentAccurate / 192
 
 
-def format_data_ml(games_data: pd.DataFrame, year: int, teams_df: pd.DataFrame):
+def format_data_ml(games_data: pd.DataFrame, year: int, league: str):
     # Populates data for past bracket results
     past_bracket_df = pd.read_csv(f"{teampath}Big_Dance_CSV.csv")
+    teams_df = pd.read_csv(f"{teampath}{league}{year}.csv", index_col="Team Name")
 
     # Populates yearlybracket_df for only one year
     yearly_bracket_df = pd.DataFrame()
@@ -129,10 +130,165 @@ class NCAADataCustom(Dataset):
         else:
             return game_data, game_label  # return data, label (X, y)
 
-train_df = yearlybracket_df.sample(frac=0.8, ignore_index=True)
-test_df = yearlybracket_df.loc[~yearlybracket_df.index.isin(train_df.index)]
+
+from torch.nn.modules.linear import Linear
+
+
+# Create a model class
+class NCAABracketModelV1(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__()
+
+        self.layer_stack_1 = nn.Sequential(
+            nn.Linear(in_features=input_shape, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
+        )
+        # self.layer_stack_2 = nn.Sequential(
+        #     nn.Linear(in_features=hidden_units, out_features=hidden_units),
+        #     nn.ReLU(),
+        #     nn.Linear(in_features=hidden_units, out_features=hidden_units),
+        #     nn.ReLU(),
+        #     nn.Linear(in_features=hidden_units, out_features=output_shape),
+        # )
+
+    # Define the forward computation (input data x flows through nn.Linear())
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # return self.layer_stack_2(self.layer_stack_1(x))
+        return self.layer_stack_1(x)
+
+
+# @title
+from tqdm.auto import tqdm
+
+
+# @title
+def train_step(model: torch.nn.Module,
+               dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer):
+    # Put model in train mode
+    model.train()
+
+    # Setup train loss and train accuracy values
+    train_loss, train_acc = 0, 0
+
+    # Loop through data loader data batches
+    for batch, (X, y) in enumerate(dataloader):
+        # Send data to target device
+        X, y = X.to(device), y.to(device)
+
+        # 1. Forward pass
+        y_pred = model(X)
+
+        # 2. Calculate  and accumulate loss
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+        # Calculate and accumulate accuracy metric across all batches
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item() / len(y_pred)
+
+    # Adjust metrics to get average loss and accuracy per batch
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+
+# 1. Take in various parameters required for training and test steps
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module = nn.CrossEntropyLoss(),
+          epochs: int = 5):
+    # 2. Create empty results dictionary
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []
+               }
+
+    # 3. Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer)
+        test_loss, test_acc = test_step(model=model,
+                                        dataloader=test_dataloader,
+                                        loss_fn=loss_fn)
+
+        # 4. Print out what's happening
+        print(
+            f"Epoch: {epoch + 1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        # 5. Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+    # 6. Return the filled results at the end of the epochs
+    return results
+
+
+# @title
+def test_step(model: torch.nn.Module,
+              dataloader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module):
+    # Put model in eval mode
+    model.eval()
+
+    # Setup test loss and test accuracy values
+    test_loss, test_acc = 0, 0
+
+    # Turn on inference context manager
+    with torch.inference_mode():
+        # Loop through DataLoader batches
+        for batch, (X, y) in enumerate(dataloader):
+            # Send data to target device
+            X, y = X.to(device), y.to(device)
+
+            # 1. Forward pass
+            test_pred_logits = model(X)
+
+            # 2. Calculate and accumulate loss
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+
+            # Calculate and accumulate accuracy
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += ((test_pred_labels == y).sum().item() / len(test_pred_labels))
+
+    # Adjust metrics to get average loss and accuracy per batch
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+
+train_df = yearly_bracket_df.sample(frac=0.8, ignore_index=True)
+test_df = yearly_bracket_df.loc[~yearly_bracket_df.index.isin(train_df.index)]
 train_data = NCAADataCustom(targ_df=train_df)
 test_data = NCAADataCustom(targ_df=test_df)
+device = "cude" if torch.cuda.is_available() else "cpu"
+device
 
 # Setup batch size and number of workers
 BATCH_SIZE = 8
@@ -150,7 +306,54 @@ test_dataloader = DataLoader(test_data,
                                     shuffle=False,
                                     num_workers=NUM_WORKERS)
 
+# Set random seeds
+# torch.manual_seed(42)
+# torch.cuda.manual_seed(42)
+# Set random seeds
+# torch.manual_seed(42)
+# torch.cuda.manual_seed(42)
 
+# Set number of epochs
+NUM_EPOCHS = 200
+
+# Recreate an instance of TinyVGG
+model_3 = NCAABracketModelV1(input_shape=4,
+                  hidden_units=8,
+                  output_shape=1).to(device)
+
+# Setup loss function and optimizer
+loss_fn = nn.L1Loss()
+optimizer = torch.optim.SGD(params=model_3.parameters(), lr=0.001)
+
+# Start the timer
+from timeit import default_timer as timer
+# Set number of epochs
+NUM_EPOCHS = 200
+
+# Recreate an instance of TinyVGG
+model_3 = NCAABracketModelV1(input_shape=4,
+                  hidden_units=8,
+                  output_shape=1).to(device)
+
+# Setup loss function and optimizer
+loss_fn = nn.L1Loss()
+optimizer = torch.optim.SGD(params=model_3.parameters(), lr=0.001)
+
+start_time = timer()
+
+# Train model_0
+model_3_results = train(model=model_3,
+                        train_dataloader=train_seeds_dataloader,
+                        test_dataloader=test_seeds_dataloader,
+                        optimizer=optimizer,
+                        loss_fn=loss_fn,
+                        epochs=NUM_EPOCHS)
+
+# End the timer and print out how long it took
+end_time = timer()
+print(f"Total training time: {end_time-start_time:.3f} seconds")
+# Start the timer
+from timeit import default_timer as timer
 # Below used for back testing ONLY
 # Gets team data from web, run once ESPN has data from 2006 - current year
 # Takes a long time to import data, ~10 min per year
